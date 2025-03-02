fa9642dc5258214c5d83548f084d9980109f8344
Akiz-Ivanov <ivanovicandreja@gmail.com>
2025-03-02 23:32:59 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',  // Allows connections from other devices
-    port: 5173,       // Default Vite port
-    strictPort: true, // Ensures the port doesn’t change
-    open: false       // Optional: prevent browser auto-open
-  }
-})
+  base: 'https://github.com/Akiz-Ivanov/Tenzies.git', // Replace with your repository name
+});
